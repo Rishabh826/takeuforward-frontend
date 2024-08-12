e889c0e826c9b1d14058d9d1bcc3877849dc44e5
Rishabh826 <rishabhjangid0707@gmail.com>
2024-08-13 00:31:22 +0530

    app change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   const fetchBannerData = async () => {
     try {
       const result = await axios.get('https://take-backend.onrender.com/api/banner');
-      console.log('Banner Data:', result.data); // Log the response
       setBannerData(result.data);
     } catch (error) {
       console.error('Error fetching banner data:', error);
@@ -20,58 +19,30 @@ function App() {
     fetchBannerData();
   }, []);
 
-  const navbarStyle = {
-    backgroundColor: '#343a40',
-    padding: '10px 0',
-  };
-  const navbarBrandStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    color: 'white',
-    textDecoration: 'none',
-  };
-
-  const navbarLogoStyle = {
-    width: '30px',
-    height: '24px',
-    marginRight: '10px',
-  };
-
-  const navbarTitleStyle = {
-    color: 'white',
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-  };
-
   return (
     <div>
-     <nav style={navbarStyle}>
-        <div>
-          <a style={navbarBrandStyle} href="#">
+      <nav style={{ backgroundColor: '#343a40', padding: '10px 0' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '1200px', margin: '0 auto' }}>
+          <a style={{ display: 'flex', alignItems: 'center', color: 'white', textDecoration: 'none' }} href="#">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS-VMhRzpkQoBeeIjjRw8mXPwMerTckBzW0FuJ_PJ02lvhVZ6ooI423PrmE5WFWYygN9E&usqp=CAU"
               alt="Logo"
-              style={navbarLogoStyle}
+              style={{ width: '30px', height: '24px', marginRight: '10px' }}
             />
-            <span style={navbarTitleStyle}>Take U Forward</span>
+            <span style={{ color: 'white', fontSize: '1.5rem', fontWeight: 'bold' }}>Take U Forward</span>
           </a>
         </div>
       </nav>
 
-      <main >
-        {bannerData ? (
+      <main style={{ flex: '1', padding: '20px' }}>
+        {bannerData && (
           <Banner
             description={bannerData.description}
             timer={bannerData.timer}
             link={bannerData.link}
           />
-        ) : (
-          <p>No banner to display</p>
         )}
-
-        <h1>Admin Dashboard</h1>
-
-        <div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
           <AdminDashboard setBannerData={setBannerData} fetchBannerData={fetchBannerData} />
         </div>
       </main>
